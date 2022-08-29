4e233ff3ed923658dc7108a11312e3d32505c45d
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-08-30 11:14:13 +1200

    Create components skeleton

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import About from './About'
+import Education from './Education'
+import Header from './Header'
+import Home from './Home'
+import Links from './Links'
+import Nav from './Nav'
+import Projects from './Projects'
+
+
 function App() {
     return (
+        <>
+        <Header/>
+        <Nav/>
         <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/education" element={<Education/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/about" element={<About/>}/>
         </Routes>
+        <Links/>
+        </>
     )
   }
   
