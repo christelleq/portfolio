9abec1ff5ffb33969e16987044a87603540f6ec2
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-08-31 10:39:54 +1200

    Edit image alt

--- client/components/Links/Link.jsx
+++ client/components/Links/Link.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Link({link, name}) {
     return (
-        <a href={link} target="_blank"><img src={`/server/public/images/`+name+`.png`} alt={name} width="40" className="pr-3.5"/></a>
+        <a href={link} target="_blank"><img src={`/server/public/images/`+name+`.png`} alt={name+` link`} width="40" className="pr-3.5"/></a>
     )
 }
