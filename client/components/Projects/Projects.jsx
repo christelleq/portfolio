8b145959017e3146b98902711814d3cf91df2851
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-09-02 11:52:59 +1200

    Complete project descriptions with responsive styling

--- client/components/Projects/Projects.jsx
+++ client/components/Projects/Projects.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Project from "./Project";
 import PortfolioDescription from "./descriptions/PortfolioDescription"
+import SolarSystemDescription from "./descriptions/SolarSystemDescription";
+import SharingIsCaringDescription from "./descriptions/SharingIsCaring";
 
 export default function Projects() {
     return (
-        <section className="pt-5 space-y-9">
+        <section className="pt-5 space-y-12 pb-12">
             <Project project="Portfolio" image="portfolio" filters={['Solo Project', 'React', 'Tailwind CSS', 'Vite']}>
                 <PortfolioDescription/>
             </Project>
-            <Project project="3D Solar System" image="solar-system" description="afkjdsfngk" filters={['Group Project', 'React Three Fiber', 'React', 'Redux', 'Vite']}/>
-            <Project project="Sharing is Caring" image="sharing-is-caring" description="afkjdsfngk" filters={['Group Project', 'React', 'Redux', 'SQLite']}/>
+            <Project project="3D Solar System" image="solar-system" description="afkjdsfngk" filters={['Group Project', 'React Three Fiber', 'React', 'Redux', 'Vite']}>
+                <SolarSystemDescription/>
+            </Project>
+            <Project project="Sharing is Caring" image="sharing-is-caring" description="afkjdsfngk" filters={['Group Project', 'React', 'Redux', 'SQLite']}>
+                <SharingIsCaringDescription/>
+            </Project>
         </section>
     )
 }
