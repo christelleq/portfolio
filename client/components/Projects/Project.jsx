1ece45065d9a81381f1071b74b401cdba474ec3b
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-09-01 14:15:46 +1200

    Apply responsive styling on projects

--- client/components/Projects/Project.jsx
+++ client/components/Projects/Project.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Project({project, image, description, tech}) {
     return (
-        <article>
-            <figure>
+        <article className="flex flex-col items-center">
+            <figure className="sm:w-[40%]">
                 <img src={`/server/public/images/`+image+`.png`} alt={project}/>
             </figure>
-            <h3>{project}</h3>
+            <h3 className="text-xl">{project}</h3>
             {/* tech stack */}
             <p>{description}</p>
         </article>
--- client/components/Projects/Projects.jsx
+++ client/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import Project from "./Project";
 
 export default function Projects() {
     return (
-        <section>
+        <section className="pt-5 space-y-9">
             <Project project="Portfolio" image="portfolio" description="afkjdsfngk" tech={[]}/>
             <Project project="3D Solar System" image="solar-system" description="afkjdsfngk" tech={[]}/>
             <Project project="Sharing is Caring" image="sharing-is-caring" description="afkjdsfngk" tech={[]}/>
