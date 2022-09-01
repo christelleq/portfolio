00811ef75ba4d6615de688b0c6d4b89ef9ee8f59
Christelle Quilang <christelle.quilang@yahoo.co.nz>
2022-09-02 11:40:23 +1200

    Increase padding under links

--- client/components/Links/Links.jsx
+++ client/components/Links/Links.jsx
@@ -3,7 +3,7 @@ import Link from "./Link";
 
 export default function Links() {
     return (
-        <footer className="flex flex-row pb-10 pt-7">
+        <footer className="flex flex-row pb-20 pt-7">
             <Link name="github" link="https://github.com/christelleq"/>
             <Link name="linkedin" link="https://linkedin.com/in/christelle-quilang"/>
             <Link name="mail" link="mailto:christellequilang@protonmail.com"/>
