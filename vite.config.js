25af309eac39029e428a2541f053b051dd2f8993
Christelle Quilang <christellequilang@protonmail.com>
2022-09-05 23:24:28 +1200

    Add aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,18 @@ export default defineConfig({
       fastRefresh: process.env.NODE_ENV !== 'test',
     }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: '/images',
+        replacement: path.resolve(__dirname, '/server/public/images'),
+      },
+      {
+        find: '/files',
+        replacement: path.resolve(__dirname, '/server/public/files'),
+      },
+    ],
+  },
   server: {
     proxy: {
       '/api': {
