b4fdca8bac344ff3149feb533a734f4f64621678
Christelle Quilang <christellequilang@protonmail.com>
2022-09-05 18:43:09 +1200

    Add filters to projects section

--- client/components/Projects/Projects.jsx
+++ client/components/Projects/Projects.jsx
@@ -3,37 +3,43 @@ import Project from './Project'
 import PortfolioDescription from './Descriptions/PortfolioDescription'
 import SolarSystemDescription from './Descriptions/SolarSystemDescription'
 import SharingIsCaringDescription from './Descriptions/SharingIsCaring'
+import Filters from './Filters'
 
 export default function Projects() {
   return (
     <section className="pt-5 space-y-12 pb-12">
-      <Project
-        project="Portfolio"
-        image="portfolio"
-        filters={['Solo Project', 'React', 'Tailwind CSS', 'Vite']}
-      >
+      <Project project="Portfolio" image="portfolio">
+        <Filters
+          filters={[
+            'Solo Project',
+            'React',
+            'Tailwind CSS',
+            'Semantic HTML',
+            'Vite',
+          ]}
+        />
         <PortfolioDescription />
       </Project>
+
       <Project
         project="3D Solar System"
         image="solar-system"
         description="afkjdsfngk"
-        filters={[
-          'Group Project',
-          'React Three Fiber',
-          'React',
-          'Redux',
-          'Vite',
-        ]}
       >
+        <Filters
+          filters={['Group Project', 'React Three Fiber', 'React', 'Redux']}
+        />
         <SolarSystemDescription />
       </Project>
+
       <Project
         project="Sharing is Caring"
         image="sharing-is-caring"
         description="afkjdsfngk"
-        filters={['Group Project', 'React', 'Redux', 'SQLite']}
       >
+        <Filters
+          filters={['Group Project', 'React', 'Redux', 'External APIs']}
+        />
         <SharingIsCaringDescription />
       </Project>
     </section>
