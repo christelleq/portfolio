d8c64aa34ce380c9fddd249960f5dda859cda390
Christelle Quilang <christellequilang@protonmail.com>
2022-09-05 16:37:00 +1200

    Complete education section

---
+++ client/components/Education/Descriptions/UniversityDescription.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function UniversityDescription() {
+  return (
+    <section className="w-[95%] sm:w-[60%] lg:w-[40%] text-slate-900">
+      <p>5 papers to completion.</p>
+      <p>
+        I focused on logic and ethics topics in philosophy, and social and
+        developmental topics in psychology. My university education enriched my
+        ability to think analytically and critically, to ask questions that
+        clarify, challenge, and deepen my understanding of complex concepts and
+        topics.
+      </p>
+      <p>
+        I also completed papers in mathematics, and complete with A grades
+        papers in first year programming and on the Māori world.
+      </p>
+    </section>
+  )
+}
