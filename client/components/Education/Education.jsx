5c1fc97e304a8d0d58b3504873970261455afa85
Christelle Quilang <christellequilang@protonmail.com>
2022-09-15 20:30:34 +1200

    Split education section into components

--- client/components/Education/Education.jsx
+++ client/components/Education/Education.jsx
@@ -1,103 +1,14 @@
 import React from 'react'
-import CertificateArticle from './CertificateArticle'
-import DevAcademyDescription from './Descriptions/DevAcademyDescription'
-import NonviolentCommunicationDescription from './Descriptions/NonviolentCommunicationDescription'
-import OptimizeDescription from './Descriptions/OptimizeDescription'
-import UniversityDescription from './Descriptions/UniversityDescription'
-import EducationArticle from './EducationArticle'
-import Skills from './Skills'
-import foundationsOfCloudComputing from '../../../server/public/files/Foundations_of_Cloud_Computing.pdf'
-import devAcademy from '../../../server/public/images/dev-academy.png'
-import uoa from '../../../server/public/images/uoa.png'
-import meditatingGiraffe from '../../../server/public/images/meditating-giraffe.png'
-import heroicCoach from '../../../server/public/images/heroic-coach.png'
-import understandingDataEngineering from '../../../server/public/files/Understanding_Data_Engineering.pdf'
+import CertificatesSection from './Sections/Certificates'
+import TertiarySection from './Sections/Tertiary'
+import TrainingSection from './Sections/Training'
 
 export default function Education() {
   return (
     <section className="pt-5 space-y-12 pb-12 pt-4">
-      <h3 className="text-2xl">Tertiary Education</h3>
-      <EducationArticle
-        education="Web Development Bootcamp"
-        place="Dev Academy Aotearoa"
-        date="Apr 2022 - Jul 2022"
-        link="https://devacademy.co.nz/"
-        image={devAcademy}
-      >
-        <Skills
-          skills={[
-            'Full Stack Software Development',
-            'Teamwork',
-            'Agile',
-            'Mindfulness',
-            'Fast learning',
-          ]}
-        />
-        <DevAcademyDescription />
-      </EducationArticle>
-
-      <EducationArticle
-        education="Philosophy & Psychology"
-        place="University of Auckland"
-        date="Feb 2017 - Nov 2021"
-        image={uoa}
-      >
-        <Skills
-          skills={[
-            'Critical thinking',
-            'Learning complex concepts',
-            'Leadership',
-          ]}
-        />
-        <UniversityDescription />
-      </EducationArticle>
-
-      <h3 className="text-2xl">Training</h3>
-      <EducationArticle
-        education="Nonviolent Communication"
-        place="Meditating Giraffe"
-        date="Jul 2021 - Sep 2022"
-        link="https://meditatinggiraffe.co.nz/"
-        image={meditatingGiraffe}
-      >
-        <Skills
-          skills={['Empathy', 'Active listening', 'Effective communication']}
-        />
-        <NonviolentCommunicationDescription />
-      </EducationArticle>
-
-      <EducationArticle
-        education="Wisdom & Wellbeing"
-        place="Heroic Coach"
-        date="Jan 2021 - Present"
-        link="https://christelle--heroicenterprises.thrivecart.com/optimize-premium/"
-        image={heroicCoach}
-      >
-        <Skills
-          skills={[
-            'Leadership',
-            'Cultivating effective habits',
-            'Effective working and living',
-          ]}
-        />
-        <OptimizeDescription />
-      </EducationArticle>
-
-      <section>
-        <h3 className="text-2xl pb-4">Certificates</h3>
-        <CertificateArticle
-          name="Foundations of Cloud Computing"
-          link={foundationsOfCloudComputing}
-          date={'Sep 2022'}
-          length={'1h'}
-        />
-        <CertificateArticle
-          name="Understanding Data Engineering"
-          link={understandingDataEngineering}
-          date={'Sep 2022'}
-          length={'2h'}
-        />
-      </section>
+      <TertiarySection />
+      <TrainingSection />
+      <CertificatesSection />
     </section>
   )
 }
