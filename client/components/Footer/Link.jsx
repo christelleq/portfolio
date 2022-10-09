dce4ede260fd7b036285de626b797246f2c6e682
Christelle Quilang <christellequilang@protonmail.com>
2022-10-09 18:27:28 +1300

    Create Vite-React app

--- client/components/Footer/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Link({ link, image, name }) {
-  return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <img src={image} alt={name + ` link`} width="40" className="pr-3.5" />
-    </a>
-  )
-}
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      fastRefresh: process.env.NODE_ENV !== 'test',
-    }),
-  ],
-  // resolve: {
-  //   alias: [
-  //     {
-  //       find: '/images',
-  //       replacement: path.resolve(__dirname, '/server/public/images'),
-  //     },
-  //     {
-  //       find: '/files',
-  //       replacement: path.resolve(__dirname, '/server/public/files'),
-  //     },
-  //   ],
-  // },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react()]
 })
